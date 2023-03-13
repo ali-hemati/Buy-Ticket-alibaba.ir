ee3fc10a36ab034b91ee4363278beef68ab3427b
AliHemati <hemati1996@gmail.com>
2023-03-13 12:16:38 +0330

    loading page and analyse result by regex

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-# import requests
-url = 'https://www.alibaba.ir/bus/THR-IFN?departing=1401-12-26'
-# data = requests.get(url)
-# print(data.text)
-##################################
-# from requests_html import HTMLSession
-#
-# session = HTMLSession()
-# r = session.get(url)
-# r.html.render()
 from selenium import webdriver
-
+import ctypes
+import re
+url = 'https://www.alibaba.ir/bus/THR-IFN?departing=1401-12-26'
 DRIVER_PATH = 'C:\\Users\\AmirHBakan\\PycharmProjects\\Bilit\\chromedriver.exe'
-driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-driver.get(url)
-test = driver.page_source
-print(test)
+while True:
+    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+    driver.set_window_position(-10000, 0)
+    driver.get(url)
+    test = driver.page_source
+    txt = test
+    x = re.findall("صندلی باقی مانده", txt)
+    print(x)
+    if x:
+        ctypes.windll.user32.MessageBoxW(0, "findddddddddd", "ticket", 1)
+    driver.quit()
