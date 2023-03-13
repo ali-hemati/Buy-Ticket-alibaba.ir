6ff911415fd493240114a7dcd842bc45f1bdd533
AliHemati <hemati1996@gmail.com>
2023-03-13 16:17:04 +0330

    refreshing page in while

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 import ctypes
 import re
+y=[]
 url = 'https://www.alibaba.ir/bus/THR-IFN?departing=1401-12-26'
 DRIVER_PATH = 'C:\\Users\\AmirHBakan\\PycharmProjects\\Bilit\\chromedriver.exe'
+driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+driver.set_window_position(-10000, 0)
+driver.get(url)
 while True:
-    driver = webdriver.Chrome(executable_path=DRIVER_PATH)
-    driver.set_window_position(-10000, 0)
-    driver.get(url)
-    test = driver.page_source
-    txt = test
-    x = re.findall("صندلی باقی مانده", txt)
-    print(x)
-    if x:
-        ctypes.windll.user32.MessageBoxW(0, "findddddddddd", "ticket", 1)
-    driver.quit()
+    try:
+        y=[]
+        while len(y) < 2:
+            test = driver.page_source
+            txt = test
+            y = re.findall("تکمیل ظرفیت", txt)
+            print(y)
+        x = re.findall("صندلی باقی مانده", txt)
+        if x:
+            print('x is', x)
+            ctypes.windll.user32.MessageBoxW(0, "findddddddddd", "ticket", 1)
+        driver.refresh()
+    except TimeoutException:
+        print("Loading took too much time!")
+        driver.refresh()
+
+
+    # driver.quit()
